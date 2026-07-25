26bc909cb6ba915e9b30a9b48197e2e9111cb355
vijay <vijaynani505@gmail.com>
2026-07-25 11:03:50 +0530

    fault tolerance patterns

--- ecom-order-service/src/main/java/com/microservice/ecom_order_service/EcomOrderServiceApplication.java
+++ ecom-order-service/src/main/java/com/microservice/ecom_order_service/EcomOrderServiceApplication.java
@@ -3,9 +3,11 @@ package com.microservice.ecom_order_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableRetry
 public class EcomOrderServiceApplication {
 
 	public static void main(String[] args) {
--- ecom-order-service/src/main/java/com/microservice/ecom_order_service/service/OrderService.java
+++ ecom-order-service/src/main/java/com/microservice/ecom_order_service/service/OrderService.java
@@ -1,7 +1,13 @@
 package com.microservice.ecom_order_service.service;
 
 import com.microservice.ecom_order_service.client.InventoryClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +18,7 @@ import java.util.UUID;
 @Service
 public class OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final InventoryClient inventoryClient;
     private  final RestTemplate restTemplate;
     private final RestClient restClient;
@@ -22,6 +29,13 @@ public class OrderService {
         this.restClient = restClient;
     }
 
+//    @Retryable(
+//            retryFor = RuntimeException.class,
+//            maxAttempts = 3,
+//            backoff = @Backoff(delay = 20000)
+//    )
+//    @RateLimiter(name = "orderService", fallbackMethod = "fallbackMethod")
+    @CircuitBreaker(name = "inventoryServiceCircuitBreaker", fallbackMethod = "fallbackMethodCircuitBreaker" )
     public String placeOrder(long productId){
 
  //       String response = restTemplate.getForObject("http://localhost:8081/inventory/"+productId, String.class);
@@ -33,6 +47,16 @@ public class OrderService {
 //                .uri("http://localhost:8081/inventory/"+productId)
 //                .retrieve().toEntity(String.class);
 //        System.out.println(res.getBody());
+        return "IN STOCK".equalsIgnoreCase(res) ? "Order Placed "+ UUID.randomUUID() : "Product is out of stock";
+    }
+    public String fallbackMethod(long productId, Throwable throwable){
+        log.info("fallback executed");
+        String res = inventoryClient.checkInventory(productId);
+        return "IN STOCK".equalsIgnoreCase(res) ? "Order Placed "+ UUID.randomUUID() : "Product is out of stock";
+    }
+    public String fallbackMethodCircuitBreaker(long productId, Throwable throwable){
+        log.info("fallback executed");
+        String res = inventoryClient.checkInventory(productId);
         return "IN STOCK".equalsIgnoreCase(res) ? "Order Placed "+ UUID.randomUUID() : "Product is out of stock";
     }
 }
